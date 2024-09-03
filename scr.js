e992f4ef6ef40514d484043d08b3610977dc391f
LokiAsgard12 <irsyadfadhal12@gmail.com>
2024-09-03 12:16:13 +0700

    finish

--- scr.js
+++ scr.js
@@ -1,28 +1,101 @@
+//
 let color =['red','green','blue','yellow'];
-let level = 1;
+//
+let level = 0;
+//
 let colorSequences =[];
+//
+let clickPattern = [];
+//
 function nextSequences (){
+//
     var randomNumber = Math.floor(Math.random() * 4);
-
+//
     var randomChoosenColor = color[randomNumber];
-
+//
     colorSequences.push(randomChoosenColor);
-
-    
+//  
     $("#"+ randomChoosenColor).fadeOut(100).fadeIn(100);
-    
+//    
     playSound(randomChoosenColor);
+//    
     console.log("next color :" + randomChoosenColor)
+//    
     console.log(colorSequences);
+//
+    $("#level-title").text("Level "+ level);
+//
+    level++;
+//
+    clickPattern = [];
 }
-
-function playSound(color){
-    var audio = new Audio ("sounds/" + color + ".mp3");
+//
+function playSound(name){
+//  
+    var audio = new Audio ("sounds/" + name + ".mp3");
+//  
     audio.play();
 }
-function startGame(){
-nextSequences();
-}
-$("#start-button").click(function(){
-    startGame();
-})
+//
+    function animatedPress(currentColor) {  
+// 
+       $("." + currentColor).addClass("pressed");
+//        
+        setTimeout(() => {
+            $("." + currentColor).removeClass("pressed");
+        }, 100);
+    }
+//
+    function startGame(){
+    nextSequences();
+    }
+//
+    function checkAnswer(currentLevel){
+//
+        if(clickPattern[currentLevel]=== colorSequences[currentLevel]){
+            console.log("Succes");
+            //
+            if(currentLevel === colorSequences.length - 1){
+                setTimeout(()=>{
+                    nextSequences();
+                }, 1000);
+            }
+            //
+        }else{
+            console.log("Wrong");
+            //
+            $("body").addClass("game-over");
+//
+            setTimeout(() => {
+                $("body").removeClass("game-over");
+            }, 200);
+//        
+            $("#level-title").text("Game Over, Refresh");
+//        
+            startOver();
+        }
+    }
+//
+    function startOver() { 
+        level =0;
+        colorSequences = [];
+        clickPattern = [];
+     }
+//
+        $("#start-button").click(function(){
+            startGame();
+        });
+//
+        $(".btn").click(function (e) { 
+            var userChoosenColor = e.target.id;
+            //
+            clickPattern.push(userChoosenColor);
+            //
+            console.log(clickPattern);
+            //            
+            playSound(userChoosenColor);
+            //
+            animatedPress(userChoosenColor);
+            //
+            checkAnswer(clickPattern.length-1);
+        });
